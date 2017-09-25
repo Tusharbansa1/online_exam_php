777a2b05f6af936aa549e145509a94cd0676b495
tushar bansal <tuskibansal@gmail.com>
2017-09-25 14:02:19 +0530

    solvepaper done

--- student/solvepaper.php
+++ student/solvepaper.php
@@ -10,14 +10,7 @@ $mysqli = new mysqli('localhost' ,'root' ,'12345' , 'exam');
 	$code = $_SESSION["code"];
 	$question_no = $_SESSION["question_no"] ;
 	//echo $question_no;
-	echo "<br>";
-		for($i=0;$i<=$question_no;$i++){
-	
-		
-		}
-		
-		
-?>
+	?>
 
 
 <!DOCTYPE html>
@@ -35,25 +28,46 @@ $mysqli = new mysqli('localhost' ,'root' ,'12345' , 'exam');
 		$count = mysqli_num_rows($result_verify);
 	echo "<br>";
 	echo "Question no. ";
-	echo $i;	
+	echo "$i";	
 	echo " ". $row[1];
 	echo "<br>";
-	echo "<input type='radio' name='$i' value='$row[2]'> $row[2] <br>";
-	// echo "option 1  *" . $row[2];
-	echo "<br>";
-	echo "<input type='radio' name='$i' value='$row[3]'> $row[3] <br>";
-	// echo "option 2  *" . $row[3];
-	echo "<br>";
-	echo "<input type='radio' name='$i' value='$row[4]'> $row[4] <br>";
-	// echo "option 3  *" . $row[4];
-	echo "<br>";
-	echo "<input type='radio' name='$i' value='$row[5]'> $row[5] <br>";
-	// echo "option 4  *" . $row[5];
-	echo "<br>";
-	echo "<input type='submit' value='Submit'> <br>";
-	}
+	?>
+	<form action='solvepaper.php' method='post'>
+	<input type='radio' name="ans<?php echo $i;?>" value='<?php echo $row[2] ?>'> <?php echo $row[2] ?> <br>
 	
+	<br>
+	<input type='radio' name="ans<?php echo $i;?>" value='<?php echo $row[3] ?>'> <?php echo $row[3] ?><br>
+	
+	
+	<br>
+	<input type='radio' name="ans<?php echo $i;?>" value='<?php echo $row[4] ?>'> <?php echo $row[4] ?><br>
+	
+	<br>
+	<input type='radio' name="ans<?php echo $i;?>" value='<?php echo $row[5] ?>'> <?php echo $row[5] ?> <br>
+	
+	<br>
+	
+	<?php
+}
+	?>
+	<input type='submit' value='submit'> <br>
+	</form>
+	<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		for($i=1;$i<=$question_no;$i++){
+		$ans  = $_POST['ans'.$i];
+		echo "<br>";
+		echo $ans ;
+		}
+	}
+	else{
+		echo "error is request";
+	}
 	?>
 	
+		
+	<?php
+	
+	?>
 </body>
 </html>
