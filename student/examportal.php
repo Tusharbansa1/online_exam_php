20955d9e443f5dbd0951aac85a3b1f4093b86702
tushar bansal <tuskibansal@gmail.com>
2017-09-25 12:03:49 +0530

    solvepaper

--- student/examportal.php
+++ student/examportal.php
@@ -7,7 +7,7 @@
 <body>
 	<?php
 	
-	 session_start();
+	session_start();
 
 	$mysqli = new mysqli('localhost' ,'root' ,'12345' , 'exam');
 
@@ -18,8 +18,21 @@
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$code = $_POST['code'];
 
-		 $_SESSION["code"] = $code;
-		header("location: solvepaper.php");
+		$sql = "SELECT id,question_no FROM `question` WHERE id='$code'";
+		$result_verify = $mysqli->query($sql);
+
+		$count = mysqli_num_rows($result_verify);
+		if($count>0){
+			$row = mysqli_fetch_assoc($result_verify);
+ 			$_SESSION["question_no"] = $row["question_no"];
+ 			
+ 			
+			$_SESSION["code"] = $code;
+			header("location: solvepaper.php");
+		}
+		else{
+			echo "this code is wrong";
+		}
 	}
 	?>
 
--- student/solvepaper.php
+++ student/solvepaper.php
@@ -8,7 +8,15 @@ $mysqli = new mysqli('localhost' ,'root' ,'12345' , 'exam');
 		die("Connection failed: " . $mysqli->connect_error);
 	}
 	$code = $_SESSION["code"];
-
+	$question_no = $_SESSION["question_no"] ;
+	//echo $question_no;
+	echo "<br>";
+		for($i=0;$i<=$question_no;$i++){
+	
+		
+		}
+		
+		
 ?>
 
 
@@ -19,6 +27,33 @@ $mysqli = new mysqli('localhost' ,'root' ,'12345' , 'exam');
 	<title>solvepaper</title>
 </head>
 <body>
+	<?php
+	for($i=1;$i<=$question_no;$i++){
+			$sql = "SELECT * FROM `setquestion` WHERE paper_id = $code and question_id='$code$i'";	
+		$result_verify = $mysqli->query($sql);
+		$row=mysqli_fetch_row($result_verify);
+		$count = mysqli_num_rows($result_verify);
+	echo "<br>";
+	echo "Question no. ";
+	echo $i;	
+	echo " ". $row[1];
+	echo "<br>";
+	echo "<input type='radio' name='$i' value='$row[2]'> $row[2] <br>";
+	// echo "option 1  *" . $row[2];
+	echo "<br>";
+	echo "<input type='radio' name='$i' value='$row[3]'> $row[3] <br>";
+	// echo "option 2  *" . $row[3];
+	echo "<br>";
+	echo "<input type='radio' name='$i' value='$row[4]'> $row[4] <br>";
+	// echo "option 3  *" . $row[4];
+	echo "<br>";
+	echo "<input type='radio' name='$i' value='$row[5]'> $row[5] <br>";
+	// echo "option 4  *" . $row[5];
+	echo "<br>";
+	echo "<input type='submit' value='Submit'> <br>";
+	}
+	
+	?>
 	
 </body>
 </html>
